cae9dd9ec3d132ba506071e03288fcf37d917b07
mdnurul <nurul.alam1@hotmail.com>
2017-06-14 22:07:01 -0400

    adds some code

--- liri.js
+++ liri.js
@@ -16,79 +16,132 @@ var userInput = process.argv[3];
 //User commands: 'my-tweets', 'soptify-this-song', 'movie-this', 'do-what-it-says'.
 //User searches:'<song name>', '<movie name>'
 
+//switch-case statement for user command
+function switchCase() {
+	switch (userCom) {
+		case 'my-tweets':
+			fetchTwitter();
+			break;
 
-//twitter done
-/*
-var params = {
-	q: 'nurulcode',
-	count: 2
+		case 'soptify-this-song':
+			fetchSpotify(userCom, userInput);
+			break;
+
+		case 'movie-this':
+			fetchMovie();
+			break;
+
+		case 'do-what-it-says':
+			fetchRandom();
+			break;
+
+			default: 
+				console.log("No matching arguments!");
+	}
 }
 
-//search two tweets from nurulcode user
+switchCase();
 
-client.get('search/tweets', params, function(error, data, response) {
-	//console.log(data);
-	if (error) {
-		return console.log(error);
+//Call twitter function
+function fetchTwitter() {
+	if (userCom) {
+		var params = {
+			q: 'nurulcode',
+			count: 2
+		}
+
+		//search two tweets from nurulcode user
+
+		client.get('search/tweets', params, function(error, data, response) {
+			//console.log(data);
+			if (error) {
+				return console.log(error);
+			}
+			var tweets = data.statuses;
+			for (var i = 0; i < tweets.length; i++) {
+				console.log("-----------------------------")
+				console.log(tweets[i].text);
+				console.log("-----------------------------")
+			}
+		});
+		fs.appendFile('log.txt', '\n-----------\n' , function(err) {});
 	}
-	var tweets = data.statuses;
-	for (var i =0; i < tweets.length; i++) {
-		console.log(tweets[i].text);
+}
+//fetchTwitter();
+
+function fetchSpotify(userCom, userInput) {
+	if (userInput === undefined) {
+			var artistUndefined = "Ace of Bass";
+			var songUndefined = "The Sign";
+			console.log(artistUndefined);
+			console.log(songUndefined);
 	}
-});
-*/
-var spotify = require('spotify');
+	else {
+		spotify.search({type: 'track', query: userInput}, function(error, response, body) {
+    		
+			if (error) {
+	       		console.log('Error occurred: ' + err);
+	       		return;
+	   		}
+	   		else {
+	   			var json = JSON.parse(body);
+				console.log("----------------------------------")
+				console.log("artist: " + json.artist);
+				console.log("Song name: " + JSON.parse(body).name);
+				console.log("url: " + JSON.parse(body).url);
+				console.log("Album: " + JSON.parse(body).album);
+				console.log("-----------------------------------")
+	   		}
+		})
+	}
+}
+/*
+//Call spotify function
+function fetchSpotify() {
+		//var songPassTo;
+	//if (!userInput) {
+		//songPassTo = "The sign";
+	//} else {
+		//songPassTo = userInput;
+	//};
+	//console.log(songPassTo)
+	request('https://api.spotify.com/v1/search?q=' + userInput + '&type=track&market=US&limit=10', function(error, response, body) {
+		if (userInput != undefined) {
+			if (error) {
+				return console.log("Error occurred: " + error)
+			}
 
-request('https://api.spotify.com/v1/tracks/search' + userInput, function(error, response, body) {
-	var json = JSON.parse(body);
-
-	console.log("artist: " + json.artist);
-	console.log("Song name: " + JSON.parse(body).name);
-	console.log("url: " + JSON.parse(body).url);
-	console.log("Album: " + JSON.parse(body).album);
-
+			var json = JSON.parse(body);
+				console.log("----------------------------------")
+				console.log("artist: " + json.artist);
+				console.log("Song name: " + JSON.parse(body).name);
+				console.log("url: " + JSON.parse(body).url);
+				console.log("Album: " + JSON.parse(body).album);
+				console.log("-----------------------------------")
+		}
   });
-
-
-//spotify this song
-/*
-var spotify = require('spotify');
-var trackName = '';
- 
-spotify.search({ type: 'track', query: trackName }, function(err, response, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
- 	
-
-    // Do something with 'data' 
-    //var data = tracks.items.track;
-    //console.log(response);
-
-    var songs = data.tracks.items.track.album.name;
-    console.log(songs);
-    	/*
-	    for(var i = 0; i < songs.length; i++){
-	    	console.log('artist(s): ' + songs[i].artists.name);
-	    	console.log('song name: ' + songs[i].name);
-	    	console.log('preview song: ' + songs[i].preview_url);
-	    	console.log('album: ' + songs[i].album.name);
-	    	console.log('-----------------------------------');
-	    } 
-});
+	fs.appendFile('log.txt', '\n-----------------\n\n', function(err) {});
+}
+//fetchSpotify();
 */
 
-/*
-//var movieName = '';
-var requestUrl = "http://www.omdbapi.com/?t=" + userInput + "&apikey=40e9cece";
+//Call movie function
 
-request(requestUrl, function(error, response, body) {
-	if (error) {
-		return console.log(error);
+function fetchMovie() {
+
+	if (userInput === undefined) {
+		var movieName = "Mr. Nobody";
+		console.log(movieName);
 	}
 
-	const json = JSON.parse(body);
+	var requestUrl = "http://www.omdbapi.com/?t=" + userInput + "&apikey=40e9cece";
+
+	request(requestUrl, function(error, response, body) {
+		if (error) {
+			return console.log(error);
+		}
+
+		const json = JSON.parse(body);
 
 			console.log("---------------------------------------------");
 			console.log("\nMy Movie: ");
@@ -102,22 +155,31 @@ request(requestUrl, function(error, response, body) {
 	   		console.log("Rotten Tomatoes: " + json.Value);
 	   		console.log("URL: https://www.rottentomatoes.com/search/?search=" + userInput);
 			console.log("-----------------------------------------------");
-});
+	});
+	fs.appendFile('log.txt', '\n-----------------\n\n', function(err) {});
+}
+//fetchMovie();
 
-//var file = require('./random.txt')
-var fileName = ("random.txt")
-fs.readFile(fileName, 'utf8', function(err, fileContents){
+//Call random function
+
+function fetchRandom() {
+	var fileName = ("random.txt")
+	fs.readFile(fileName, 'utf8', function(err, fileContents){
 		if (err) {
 			return console.log(err)
 		} else {
-			console.log(fileContents);
-			//var array = data.split(',');
-
+			var textArray = fileContents.split(',');
 			//userCom = array[0];
-			//userSearch = array [1];
+			//userInput = array [1];
+			fetchSpotify(textArray[1]);
 
-			//switchCase();
+			console.log("----------------------")
+			console.log(userInput);
+			console.log("----------------------")
 		}
 	});
+	fs.appendFile('log.txt', '\n-----------------\n\n', function(err) {});
+}
+//fetchRandom();
 
-*/
+
