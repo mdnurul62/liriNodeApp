a8434a8d939246b93385e08fefaa3f5753a3223b
mdnurul <nurul.alam1@hotmail.com>
2017-06-14 23:50:23 -0400

    final

--- liri.js
+++ liri.js
@@ -24,7 +24,7 @@ function switchCase() {
 			break;
 
 		case 'soptify-this-song':
-			fetchSpotify(userCom, userInput);
+			fetchSpotify();
 			break;
 
 		case 'movie-this':
@@ -36,9 +36,9 @@ function switchCase() {
 			break;
 
 			default: 
-				console.log("No matching arguments!");
+			console.log("No matching arguments!");
 	}
-}
+};
 
 switchCase();
 
@@ -64,101 +64,74 @@ function fetchTwitter() {
 				console.log("-----------------------------")
 			}
 		});
-		fs.appendFile('log.txt', '\n-----------\n' , function(err) {});
+		var myFirstTwit = 'fake tweets'
+		fs.appendFile('log.txt', myFirstTwit , function(err) {});
 	}
 }
-//fetchTwitter();
 
-function fetchSpotify(userCom, userInput) {
-	if (userInput === undefined) {
-			var artistUndefined = "Ace of Bass";
-			var songUndefined = "The Sign";
-			console.log(artistUndefined);
-			console.log(songUndefined);
-	}
-	else {
-		spotify.search({type: 'track', query: userInput}, function(error, response, body) {
+
+//Call spotify function
+
+function fetchSpotify() {
+	if (!userInput) {
+	   	var artistUndefined = "Ace of Bass";
+		var songUndefined = "The Sign";
+		console.log(artistUndefined);
+		console.log(songUndefined);
+	} else {
+		var qUrl = "https://api.spotify.com/v1/search?type=" + userInput
+		request(qUrl, function(error, response, body) {
     		
 			if (error) {
 	       		console.log('Error occurred: ' + err);
 	       		return;
+	   		} else {
+		   			var json = JSON.parse(body);
+						console.log("----------------------------------")
+						console.log("artist: " + json.artist);
+						console.log("Song name: " + JSON.parse(body).name);
+						console.log("url: " + JSON.parse(body).url);
+						console.log("Album: " + JSON.parse(body).album);
+						console.log("-----------------------------------")
+	   			}
 	   		}
-	   		else {
-	   			var json = JSON.parse(body);
-				console.log("----------------------------------")
-				console.log("artist: " + json.artist);
-				console.log("Song name: " + JSON.parse(body).name);
-				console.log("url: " + JSON.parse(body).url);
-				console.log("Album: " + JSON.parse(body).album);
-				console.log("-----------------------------------")
-	   		}
-		})
+		);
+		
 	}
 }
-/*
-//Call spotify function
-function fetchSpotify() {
-		//var songPassTo;
-	//if (!userInput) {
-		//songPassTo = "The sign";
-	//} else {
-		//songPassTo = userInput;
-	//};
-	//console.log(songPassTo)
-	request('https://api.spotify.com/v1/search?q=' + userInput + '&type=track&market=US&limit=10', function(error, response, body) {
-		if (userInput != undefined) {
-			if (error) {
-				return console.log("Error occurred: " + error)
-			}
-
-			var json = JSON.parse(body);
-				console.log("----------------------------------")
-				console.log("artist: " + json.artist);
-				console.log("Song name: " + JSON.parse(body).name);
-				console.log("url: " + JSON.parse(body).url);
-				console.log("Album: " + JSON.parse(body).album);
-				console.log("-----------------------------------")
-		}
-  });
-	fs.appendFile('log.txt', '\n-----------------\n\n', function(err) {});
-}
-//fetchSpotify();
-*/
 
 //Call movie function
 
 function fetchMovie() {
+	if (!userInput) {
+			var movieName = "Mr. Nobody";
+			console.log(movieName);
+	} else {		
+		var requestUrl = "http://www.omdbapi.com/?t=" + userInput + "&apikey=40e9cece";
 
-	if (userInput === undefined) {
-		var movieName = "Mr. Nobody";
-		console.log(movieName);
-	}
-
-	var requestUrl = "http://www.omdbapi.com/?t=" + userInput + "&apikey=40e9cece";
-
-	request(requestUrl, function(error, response, body) {
-		if (error) {
+		request(requestUrl, function(error, response, body) {
+			if (error) {
 			return console.log(error);
-		}
+			} 
 
-		const json = JSON.parse(body);
+			const json = JSON.parse(body);
 
-			console.log("---------------------------------------------");
-			console.log("\nMy Movie: ");
-	   		console.log("\nTitle: " + json.Title);
-	   		console.log("Year: " + json.Year);
-	   		console.log("Rated: " + json.imdbRating);
-	   		console.log("Country: " + json.Country);
-	   		console.log("Language: " + json.Language);
-	   		console.log("Plot: " + json.Plot);
-	   		console.log("Actors: " + json.Actors);
-	   		console.log("Rotten Tomatoes: " + json.Value);
-	   		console.log("URL: https://www.rottentomatoes.com/search/?search=" + userInput);
-			console.log("-----------------------------------------------");
-	});
+				console.log("---------------------------------------------");
+				console.log("\nMy Movie: ");
+		   		console.log("\nTitle: " + json.Title);
+		   		console.log("Year: " + json.Year);
+		   		console.log("Rated: " + json.imdbRating);
+		   		console.log("Country: " + json.Country);
+		   		console.log("Language: " + json.Language);
+		   		console.log("Plot: " + json.Plot);
+		   		console.log("Actors: " + json.Actors);
+		   		console.log("Rotten Tomatoes: " + json.Value);
+		   		console.log("URL: https://www.rottentomatoes.com/search/?search=" + userInput);
+				console.log("-----------------------------------------------");
+		});
+	}
 	fs.appendFile('log.txt', '\n-----------------\n\n', function(err) {});
 }
-//fetchMovie();
 
 //Call random function
 
@@ -180,6 +153,4 @@ function fetchRandom() {
 	});
 	fs.appendFile('log.txt', '\n-----------------\n\n', function(err) {});
 }
-//fetchRandom();
-
 
